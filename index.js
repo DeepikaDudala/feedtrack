cbdcf0f23571d238f9f1d214c471222278d32d22
deepika dudala <deepika.dudala>
2023-11-06 18:34:33 +0530

    response sent

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config({ path: "./config.env" });
 const app = express();
-const PORT = process.env.PORT;
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`the server is running at port ${PORT}`);
 });
